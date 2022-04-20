d6530f49c48d53662b667cd8e683bf384269c9a3
linyana <1439713475@qq.com>
2022-04-20 18:49:23 +0800

    update

--- src/pages/Information.vue
+++ src/pages/Information.vue
@@ -1,15 +1,71 @@
 <template>
-    <div>
-        
+    <div id="information">
+        <div class="line">
+            <div class="left">网商银行将暂停支付宝提现 支付宝: 其他银行服务不变</div>
+            <div class="right">2022.4.20</div>
+        </div>
+        <div class="line">
+            <div class="left">网商银行将暂停支付宝提现 支付宝: 其他银行服务不变</div>
+            <div class="right">2022.4.20</div>
+        </div>
+        <div class="line">
+            <div class="left">网商银行将暂停支付宝提现 支付宝: 其他银行服务不变</div>
+            <div class="right">2022.4.20</div>
+        </div>
+        <div class="line">
+            <div class="left">网商银行将暂停支付宝提现 支付宝: 其他银行服务不变</div>
+            <div class="right">2022.4.20</div>
+        </div>
+        <div class="line">
+            <div class="left">网商银行将暂停支付宝提现 支付宝: 其他银行服务不变</div>
+            <div class="right">2022.4.20</div>
+        </div>
+        <div class="line">
+            <div class="left">网商银行将暂停支付宝提现 支付宝: 其他银行服务不变</div>
+            <div class="right">2022.4.20</div>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style scoped>
+#information {
+    width: 800px;
+    margin: auto;
+}
 
+.line {
+    cursor: pointer;
+    position: relative;
+    width: 800px;
+    height: 50px;
+    line-height: 50px;
+    font-size: 20px;
+    color: rgb(78, 75, 75);
+    padding: 5px;
+}
+
+.line:hover {
+    color: white;
+    background-color: rgb(201, 201, 201);
+} 
+
+.left {
+    position: absolute;
+    left: 0;
+    width: 680px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    text-indent: 5px;
+}
+
+.right {
+    position: absolute;
+    right: 0;
+    width: 100px;
+}
 </style>
